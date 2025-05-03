cdf1d540d13a4f0d48bdcb72dec48afc7d0dab66
Ivan Malishevskyi <ivanmalishevskiy0@gmail.com>
2025-05-03 04:08:33 +0200

    Some docker configurations, added frontend prototype for demonstrational purposes.

--- NB.URLShortener.API/Program.cs
+++ NB.URLShortener.API/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddSwaggerGen(setupAction =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
